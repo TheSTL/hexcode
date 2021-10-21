c67940d88e9c28ea666d1c0f552680fed49714f0
ashu <ashudeshwal999@gmail.com>
2021-10-21 13:53:32 +0530

    fix import

--- packages/Modal/ModalOverlay.tsx
+++ packages/Modal/ModalOverlay.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from '@emotion/styled';
+import styled from 'styled-components';
 import { useModalContext } from './context'
 import { Box } from '../Box'
 
